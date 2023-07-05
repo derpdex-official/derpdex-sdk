841bf9bc5251bea2fb87b887d696aed596a530ad
geckocoding <hello.derpdex@gmail.com>
2023-07-05 18:22:41 +0800

    Update the pool hash for factory contract

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
 export const POOL_INIT_CODE_HASH = '0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54'
 
-export const ZKSYNC_POOL_INIT_CODE_HASH = '0x010011b5a863aee85f9ffb9ff5152cfcd202f5f5ce21f1aeb7c57d30537ffb28'
+export const ZKSYNC_POOL_INIT_CODE_HASH = '0x010011b7519da827d39a569ad92eec6a634c6f1c0fbe1ca94ac01642821772da'
 
 /**
  * The default factory enabled fee amounts, denominated in hundredths of bips.
